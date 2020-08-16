952084db84fb067f930e285dcd103b4d361e0bca
Rahul <rs69766@gmail.com>
2020-08-16 21:55:56 +0530

    email to username changes

--- models/user.py
+++ models/user.py
@@ -5,16 +5,16 @@ class UserModel(db.Model):
 
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(80))
+    username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, email, password):
-        self.email = email
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
 
     @classmethod
-    def find_by_email(cls,email):
-        return cls.query.filter_by(email=email).first()
+    def find_by_username(cls,username):
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls,_id):
--- resources/user.py
+++ resources/user.py
@@ -6,13 +6,13 @@ from models.user import UserModel
 class  UserRegister(Resource) :
 
     parser = reqparse.RequestParser()
-    parser.add_argument('email' , type=str, required = True ,help = "email required")
+    parser.add_argument('username' , type=str, required = True ,help = "username required")
     parser.add_argument('password' , type=str, required = True ,help = "password required")
 
     def post(self):
         data = UserRegister.parser.parse_args()
         user = UserModel(**data)
-        if UserModel.find_by_email(data['email']):
-            return {"message" : " user {} already exists".format(data['email'])}, 400
+        if UserModel.find_by_username(data['username']):
+            return {"message" : " user {} already exists".format(data['username'])}, 400
         user.save_to_db()
         return {"message" : " user created successfully"},201
